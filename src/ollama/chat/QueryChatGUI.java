2ba7a88477345c296951b13784b09e6b08cc87be
111031075jack <111031075@live.asia.edu.tw>
2025-11-15 16:50:03 +0800

    update

--- src/ollama/chat/QueryChatGUI.java
+++ src/ollama/chat/QueryChatGUI.java
@@ -5,7 +5,6 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
